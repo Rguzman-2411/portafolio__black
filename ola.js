81da8edd969b3e048fb191b7e54056a2c1598cbc
Rguzman-2411 <ricardo.guzman1@uac.edu.co>
2024-06-04 05:10:59 -0500

    Add files via upload

---
+++ ola.js
@@ -0,0 +1,17 @@
+document.getElementById('black-icon').addEventListener('mouseover', function() {
+    this.src = './Recursos/Yo opc2.png'; // Cambia la imagen al pasar el cursor
+});
+
+document.getElementById('black-icon').addEventListener('mouseout', function() {
+    this.src = './Recursos/Yo opc1.png'; // Restaura la imagen original al sacar el cursor
+});
+
+
+document.querySelector('.bolsa-plastico-con-mis-trabajo').addEventListener('mouseover', function() {
+    this.src = './Recursos/bolsa plastico con mis trabajos__interactividad.png'; // Cambia la imagen al pasar el cursor
+});
+
+document.querySelector('.bolsa-plastico-con-mis-trabajo').addEventListener('mouseout', function() {
+    this.src = './Recursos/bolsa plastico con mis trabajos.png'; // Restaura la imagen original al sacar el cursor
+});
+
